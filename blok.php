85985d3d4983f5db2ad218caf86117c221d1e905
Rifqianasabhinaya <90910950+Rifqianasabhinaya@users.noreply.github.com>
2021-11-22 06:04:50 +0700

    Add files via upload

---
+++ blok.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Akses di blok</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="<?= base_url('assests/'):?>css/sb-admin-2.min.css"
+    rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Content wrapper -->
+        <div id="content=wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+            <!-- Begin Page Content-->
+            <hr class="container-fluid mt-5">
+
+            <!-- 404 Error Text-->
+            <li class="text-center">
+                <a class="error mx-auto" data-text="403">403</div>
+                    <p class="lead text-gray-800 mb-5">Akses tidak
+        diizinkan</p>
+                        <p class="text-gray-500 mb-0">Sepertinya Kamu mengakses
+        halaman yang salah</p>
+                    
+</div>
+                       
+            </div>
+            <!-- /.controller-Fluid -->
+
+</div>
+<!-- End of Main content -->
+
+</div>
+<!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+
+
+            <!-- Scroll To Top Button -->
+            <li class="scroll-to-top-rounded " href="#page-top">
+               <i class="fas fa-angle-up"><i/>
+                </a>
+
+                <!-- Logout Modal -->
+                <div class="modal fade" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Custom Components:</h6>
+                        <a class="collapse-item" href="buttons.html">Buttons</a>
+                        <a class="collapse-item" href="cards.html">Cards</a>
+                    </div>
+                </div>
+            </li>
+
+            <!-- Nav Item - Utilities Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+                    aria-expanded="true" aria-controls="collapseUtilities">
+                    <i class="fas fa-fw fa-wrench"></i>
+                    <span>Utilities</span>
+                </a>
+                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Custom Utilities:</h6>
+                        <a class="collapse-item" href="utilities-color.html">Colors</a>
+                        <a class="collapse-item" href="utilities-border.html">Borders</a>
+                        <a class="collapse-item" href="utilities-animation.html">Animations</a>
+                        <a class="collapse-item" href="utilities-other.html">Other</a>
+                    </div>
+                </div>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Addons
+            </div>
+
+           
---
+++ gagal.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Gagal aktivasi akun</title>
+
+    <!-- Custom styles for this template-->
+    <link href="<?= base_url('assets/'); ?>vendor/fontawesomre
+    <link
+        href=
+
+    <!-- Custom styles for this template-->
+    <link href="<?= base_url('assests/'):?>css/sb-admin-2.min.css"
+    rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Content wrapper -->
+        <div id="content=wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+            <!-- Begin Page Content-->
+            <hr class="container-fluid mt-5">
+
+            <!-- 404 Error Text-->
+            <li class="text-center">
+                <a class="error mx-auto" data-text="403">403</div>
+                    <p class="lead text-gray-800 mb-5">Akses tidak
+        diizinkan</p>
+                        <p class="text-gray-500 mb-0">Sepertinya Kamu mengakses
+        halaman yang salah</p>
+                    
+</div>
+                       
+            </div>
+            <!-- /.controller-Fluid -->
+
+</div>
+<!-- End of Main content -->
+
+</div>
+<!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+
+
+            <!-- Scroll To Top Button -->
+            <li class="scroll-to-top-rounded " href="#page-top">
+               <i class="fas fa-angle-up"><i/>
+                </a>
+
+                <!-- Bootstrap core Javascript -->
+                <script src="<?= base_url('assets/');
+            ?>vendor/jquery/jquery.min  .js"></script>
+                <script src="<?= base_url('assets/');
+            ?>vendor/bootstrap/js/bootstrap.bundle.min.j s"></script>
+
+                <!-- Core Plugin Javascript -->
+                <script src="<?= base_url('assets/'); ?>vendor/jquery-
+            easing/jquery.easing.min.j s"></script>
+                
+                <!-- Costum scripts for all pages -->
+                <script src="<?= base_url('assets/'); ?>js/sb-admin-
+            2.min.js"></script>
+                    <script>
+                        $('.alert-message').alert().delay(3500).slideUp('slow');
+                    </script>
+
+</body>
+
+</html>
+
+                 
+                                  
